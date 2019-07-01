37621b4c5717e9b97d41008e3d57845f43ef609b
SoulDundy <44551402+SoulDundy@users.noreply.github.com>
2019-07-01 01:04:01 -0300

    Add files via upload

---
+++ FourierSeries/complex.js
@@ -0,0 +1,58 @@
+class Complex {
+	constructor(real, imaginary) {
+		this.real = real || 0;
+		this.imaginary = imaginary || 0;
+	}
+
+	static add(a, b) {
+		return new Complex(a.real + b.real, a.imaginary + b.imaginary);
+	}
+
+	add(b) {
+		this.real += b.real;
+		this.imaginary += b.imaginary;
+		return this;
+	}
+
+	mult(b) {
+		if (b instanceof Complex) {
+			const tempReal = this.real;
+			this.real = this.real * b.real - this.imaginary * b.imaginary;
+			this.imaginary = tempReal * b.imaginary + this.imaginary * b.real;
+		} else {
+			this.real *= b;
+			this.imaginary *= b;
+		}
+		return this;
+	}
+
+	static mult(a, b) {
+		const result = new Complex();
+		if (b instanceof Complex) {
+			const tempReal = a.real;
+			result.real = a.real * b.real - a.imaginary * b.imaginary;
+			result.imaginary = tempReal * b.imaginary + a.imaginary * b.real;
+		} else {
+			result.real *= b;
+			result.imaginary *= b;
+		}
+
+		if (Math.abs(result.imaginary) < 1e-4) result.imaginary = 0;
+		return result;
+	}
+
+	static exp(b) {
+		const result = new Complex(Math.cos(b.imaginary), Math.sin(b.imaginary));
+		result.mult(Math.exp(b.real));
+		if (Math.abs(result.imaginary) < 1e-4) result.imaginary = 0;
+		return result;
+	}
+
+	getMagnitude() {
+		return Math.sqrt(this.real * this.real + this.imaginary * this.imaginary);
+	}
+
+	copy() {
+		return new Complex(this.real, this.imaginary);
+	}
+}
