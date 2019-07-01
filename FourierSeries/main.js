bf5aeac343c05fc3923f15b7cca214b33b49c2e8
SoulDundy <44551402+SoulDundy@users.noreply.github.com>
2019-07-01 13:14:28 -0300

    Add files via upload

--- FourierSeries/main.js
+++ FourierSeries/main.js
@@ -1,35 +1,61 @@
-const range = 1.2;
+const range = 1.5;
 const coefficients = [];
 let terms = [];
 let scalingFactor;
 
 let t = 0;
 
-const numOfTerms = 30; // Actual number of terms is 2 * numOfterms + 1;
+const numOfTerms = 100; // Actual number of terms is 2 * numOfterms + 1;
 
-let trailX = [];
-let trailY = [];
+let trail = [];
 
 let fileData;
 let coords = [];
 
 function preload() {
 	fileData = loadStrings("coords.txt");
+	pathData = loadJSON("path.json");
 }
 
 function setup() {
 	createCanvas(720, 720);
 	ellipseMode(RADIUS);
 
-	for (let i = 0; i < fileData.length; i++) {
-		const splitted = fileData[i].split(' ');
-		const num = {
-			real: float(splitted[0]),
-			imaginary: float(splitted[1]),
-		}
-		if (!isNaN(num.real))
-			coords.push(num);
+	while (pathData.drawing.length != 1) {
+		pathData.drawing[0] = pathData.drawing[0].concat(pathData.drawing[1]);
+		pathData.drawing.splice(1, 1);
 	}
+
+	pathData.drawing = pathData.drawing[0];
+
+	for (let i = 2; i < pathData.drawing.length; i++) {
+		pathData.drawing[0] = pathData.drawing[0].concat(pathData.drawing[i]);
+		pathData.drawing.splice(i, 1);
+	}
+
+	while (pathData.drawing.length !== 2) {
+		pathData.drawing[1] = pathData.drawing[1].concat(pathData.drawing[2]);
+		pathData.drawing.splice(2, 1);
+	}
+
+	for (let i = 0; i < pathData.drawing[0].length; i++) {
+		coords.push({
+			real: map(pathData.drawing[0][i], 0, 255, -1, 1),
+			imaginary: map(pathData.drawing[1][i], 0, 255, 1, -1)
+		})
+	}
+
+
+	// for (let i = 0; i < fileData.length; i++) {
+	// 	fileData[i] = fileData[i].trim();
+	// 	const splitted = fileData[i].split(' ');
+	// 	const num = {
+	// 		real: float(splitted[0]),
+	// 		imaginary: float(splitted[1]),
+	// 	}
+	// 	if (!isNaN(num.real))
+	// 		coords.push(num);
+	// }
 
 	scalingFactor = width / (2 * range);
 
@@ -45,7 +71,7 @@ function draw() {
 	background(51);
 
 	// Calculate each term of the sequence.
-	// Tn = e ^ (tau * i * n * t) * Cn
+	// Nth Term = e ^ (tau * i * n * t) * Nth Coefficient
 	for (let n = 0; n < coefficients.length; n++) {
 		const exponential = Complex.exp(new Complex(0, TAU * (n - numOfTerms) * t));
 		terms[n] = {
@@ -88,24 +114,23 @@ function draw() {
 	}
 
 	// Draw trail.
-	if (t <= 1.1 && t > 0) {
-		trailX.unshift(sum.real);
-		trailY.unshift(sum.imaginary);
-		// trailY.unshift(0);
+	if (t <= 1.5 && t > 0) {
+		trail.unshift({
+			real: sum.real,
+			imaginary: sum.imaginary
+		});
 	}
 
 	noFill();
 	stroke(255, 255, 0);
 	beginShape();
-	for (let i = trailX.length - 1; i >= 0; i--) {
-		// vertex(xToCanvas(trailX[i]), (trailY[i]) + height / 2);
-		vertex(xToCanvas(trailX[i]), yToCanvas(trailY[i]));
-		// trailY[i]++;
+	for (let i = trail.length - 1; i >= 0; i--) {
+		vertex(xToCanvas(trail[i].real), yToCanvas(trail[i].imaginary));
 	}
 	endShape();
 
 	if (frameRate() !== 0) {
-		t += 1 / (frameRate() * 10);
+		t += 1 / (frameRate() * 9.8);
 	}
 }
 
@@ -132,7 +157,7 @@ function stepFunction(n) {
 
 function coefficient(func, n) {
 	const newFunc = k => Complex.mult(func(k), Complex.exp(new Complex(0, -TAU * k * n)));
-	return integral(newFunc, 0, 1, 1e4);
+	return integral(newFunc, 0, 1, 20000)
 }
 
 function integral2(func, xMin, xMax, stepSize) {
